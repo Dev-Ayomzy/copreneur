201072948272bb36634a29803e3e6d60ba214bc3
yusuf abdulbasit <yusufabdulbasit316@gmail.com>
2025-08-27 07:17:08 -0700

    bare project

---
+++ app/index.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function index (){
+  return (
+    <view style={{
+      flex: 1,
+      backgroundColor: "brown"
+    }}>
+        <View style={{
+            flex: 2,
+            backgroundColor: "white",
+        }}>
+            <Text>coprenuer</Text>
+            <Text>Where developer meet entreprenuer </Text>
+            </View>
+
+            <View style={{
+              flex: 2,
+              backgroundColor: "yellow",
+            }}>
+                <Text style={style.p}>I am a entreprenuer</Text>
+            </View>
+
+            <View style={{
+              flex: 2,
+              backgroundColor: "oldlace",
+            }}>
+                <Text style={style.p}>I am a developer</Text>
+              </View>
+        </view>
+  )  
+}
+
+const styles = StyleSheet.create({
+  p: {
+    fontSize: 16,
+    fontWeight: "bold",
+  }
+})
