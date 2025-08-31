722c4463791a4da66c8a8c6d8fed47d558f51d02
Dev-Ayomzy <yusufabdulbasit316@gmail.com>
2025-08-31 13:14:35 -0700

    sign up screen update

--- app/signup.js
+++ app/signup.js
@@ -1,9 +1,42 @@
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { StatusBar } from "react-native-web";
+import { colors } from "../theme/colors";
 
 export default function Signup () {
-    return (
-        <View>
-            <Text>Sign up screen</Text>
+    return(
+        <View style={style.wrapper}> 
+            {/* header group */}
+            <View style={Styles.header}>
+                <Text style={Styles.brandName}>copreneur</Text>
+                <Text style={Styles.brandDesc}>Where entrepreneurs collaborate with developer</Text>
+            </View>
+
+            {/* {body group} */}
+            <View>
+                <Text>Create an account</Text>
+                <TouchableOpacity>
+                    <Image
+                    style={{
+                        width: 36,
+                        height: 36
+                    }}
+                    source={require("../assets/images/google.png")}
+                    />
+                </TouchableOpacity>
+            </View>
+
+            {/* bottom grouo */}
+            <View></View>
         </View>
-    )
+    );
 }
+
+const Styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        getBackgroundColor: colors.brown200,
+        paddingTop: StatusBar.currentHeight,
+        paddingHorizontal: 16
+    },
+})
+
